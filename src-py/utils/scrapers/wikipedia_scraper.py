3ef595e47748bddd41359185f8b70f9ed617cf59
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-08-30 10:33:27 +0200

    feat: abstracted wikipedia scraper into separate file

---
+++ src-py/utils/scrapers/wikipedia_scraper.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium import webdriver
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class WikipediaScraper():
+    driver: webdriver.Chrome
+    BIO_CONTAINER_SELECTORS = [
+        "table.infobox.biography.vcard", "table.infobox.vcard"]
+
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+    def find_bio_container_wrapper(self):
+        for selector in self.BIO_CONTAINER_SELECTORS:
+            try:
+                return WebDriverWait(self.driver, 2).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                continue
+        return None
+
+    def find_bio_image_url(self, bio_container_wrapper):
+        img = bio_container_wrapper.find_element(
+            By.CSS_SELECTOR, "img.mw-file-element"
+        )
+        return img.get_attribute("src")
